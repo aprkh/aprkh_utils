4259326367a946266ff80922c5c11839f1251fca
aprkh <68245961+aprkh@users.noreply.github.com>
2024-10-04 11:45:28 -0400

    fixed bugs blah

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 from .seqio import write_fasta
 from . import stringops
 from . import misc
-from .listops import batch
+from .listops import batch_list
 
 from .decorators import fn_timer
 from .decorators import delay
--- listops.py
+++ listops.py
@@ -3,18 +3,12 @@
 import math 
 
 
-def batch_list(A, n=None, nbatches=None):
+def batch_list(A, n):
     """
     Splits the list A into batches of size n. 
-    If nbatches is specified, splits A evenly into nbatches batches. 
     """
-    if nbatches is not None:
-        n = len(A) // nbatches
-    else:
-        nbatches = math.ceil(A / n)
-
+    nbatches = math.ceil(len(A) / n)
     out = [None for _ in range(nbatches)]
     for i, j in enumerate(range(0, len(A), n)):
         out[i] = A[j:j+n]
-
     return out
