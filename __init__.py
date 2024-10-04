db83ad272a5ca27ef6be7303a153576f89c0d6e7
aprkh <68245961+aprkh@users.noreply.github.com>
2024-10-04 18:29:15 -0400

    changed package structure; added utility for batching computations on list

--- __init__.py
+++ __init__.py
@@ -4,8 +4,9 @@ from . import stringops
 from . import misc
 from .listops import batch_list
 
-from .decorators import fn_timer
-from .decorators import delay
+#from .decorators import delay
+#from .decorators import fn_timer
+#from .decorators import batch_comp_on_list
 
 
-__all__ = ['write_fasta', 'stringops', 'misc', 'batch_list', 'delay', 'fn_timer']
+__all__ = ['write_fasta', 'stringops', 'misc', 'batch_list']
