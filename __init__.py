3038f2543bf99c1995ab591debe3d39d4ccd8e6e
aprkh <68245961+aprkh@users.noreply.github.com>
2024-10-05 14:00:18 -0400

    fixed package structure

--- __init__.py
+++ __init__.py
@@ -4,9 +4,7 @@ from . import stringops
 from . import misc
 from .listops import batch_list
 
-#from .decorators import delay
-#from .decorators import fn_timer
-#from .decorators import batch_comp_on_list
+from . import decorators
 
 
 __all__ = ['write_fasta', 'stringops', 'misc', 'batch_list']
